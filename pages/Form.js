7b240b1081e8acc092a31bbe44a06660c0a509ce
G2N1337 <groovestreet654@gmail.com>
2022-04-26 00:13:44 +0700

    fixed

--- pages/Form.js
+++ pages/Form.js
@@ -166,7 +166,7 @@ const CreditForm = () => {
 								console.log('success');
 								setError(undefined);
 								axios
-									.post('http://localhost:5000/cards/create', {
+									.post('http://localhost:3000/card', {
 										cardNumber: cardNumber,
 										cvv: cvv,
 										date: date,
